b3a88f46cf66a03230ee560415be615a067b751a
A. N. M. Bazlur Rahman <bazlur@therapservices.net>
2014-10-21 17:34:50 +0600

    Test commit : add user

---
+++ src/test/java/com/therapjavafest/chatter/repository/UserDaoTest.java
@@ -0,0 +1,30 @@
+package com.therapjavafest.chatter.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveUser() throws Exception {
+
+    }
+
+    @Test
+    public void testFindUserByUserName() throws Exception {
+
+    }
+}
